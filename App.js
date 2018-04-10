150a45291aa2d59fa76c3eaa24cc7f6e8fb3c9b1
Mithilesh Tarkar <mithilesh.tarkar@ves.ac.in>
2018-04-10 16:14:14 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import codePush from 'react-native-code-push';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component{
+
+  componentDidMount()
+  {
+			
+	  	codePush.sync({ updateDialog:true,installMode:codePush.InstallMode.IMMEDIATE});
+  } 
+
+  render() {
+    const input1 = this.state.input1;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Mithilesh Tarkar 
+          Welcome to React Native!
+        </Text>
+        <View>
+                <TouchableOpacity onPress={this.onButtonPress}>
+                    <Text>Check for updates</Text>
+                </TouchableOpacity>
+            </View> 
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+App = codePush(codePushOptions)(App);
